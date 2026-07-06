43de195d5922a1754223ef310b64e6b6e3d506d1
Sarvesh <susindransarvesh@gmail.com>
2026-07-06 15:56:47 +0530

    Project 01

---
+++ Tasks/temperature_converter.py
@@ -0,0 +1,5 @@
+print("Celcius to Fahrenheit Converter")
+# (0°C × 9/5) + 32
+Celcius = int (input("Enter Celcius that needs to be converted : "))
+fahrenheit = (Celcius *9/5) +32
+print(f"The temperature in Fahrenheit is {fahrenheit}")
